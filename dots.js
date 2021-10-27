ddb7694a61f68d3652c715193649a4907c0628c1
xokcton <qwertyman2023@gmail.com>
2021-10-27 21:35:39 +0300

    dots task

---
+++ dots.js
@@ -0,0 +1,22 @@
+const getPossibleCombinations = (arr, letter) => {
+  const combinations = []
+
+  for (let index = 0; index < arr.length; index++) {
+    combinations.push(`${arr[index]}${letter}`)
+    combinations.push(`${arr[index]}.${letter}`)
+  }
+
+  return combinations
+}
+
+const insertDots = (arr) => {
+  let tmp = [arr[0]]
+
+  for (let index = 1; index < arr.length; index++) {
+    tmp = [...getPossibleCombinations(tmp, arr[index])]
+  }
+
+  return tmp
+}
+
+console.log(insertDots('abсd'))
