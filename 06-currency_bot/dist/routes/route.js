b9c71476bf3e430da633932bc34e5da946ddea85
xokcton <qwertyman2023@gmail.com>
2021-12-12 19:49:58 +0200

    added task6 - tg bot

---
+++ 06-currency_bot/dist/routes/route.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const telegram_1 = require("../controllers/telegram");
+const router = (0, express_1.Router)();
+router.post('/bot', telegram_1.getMessage);
+exports.default = router;
---
+++ 06-currency_bot/src/routes/route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import { getMessage } from '../controllers/telegram'
+
+const router = Router()
+
+router.post('/bot', getMessage)
+
+export default router
